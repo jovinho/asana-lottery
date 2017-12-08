97d8203ba1d394ef435de78a27348db72fe82472
Leonardo Maia <leonardo.maia.davila@gmail.com>
2017-12-08 19:29:08 -0200

    home pronta

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
-
 import './App.css';
+import { Hero, Container, Title, SubTitle, Button } from 'reactbulma'
 
 class App extends Component {
   render() {
     return (
-			<h1> oi </h1>
+			<Hero dark fullheight>
+				<Hero.Body>
+					<Container className="main-container">
+						<img className="derose-logo" src="/images/derose.png" alt="DeRose Method" />
+						<Title className="main-title">
+							Sorteio de Ásanas
+						</Title>
+						<SubTitle>
+							<Button large className="main-button">Começar</Button>
+						</SubTitle>
+					</Container>
+				</Hero.Body>
+			</Hero>
 		)
   }
 }
